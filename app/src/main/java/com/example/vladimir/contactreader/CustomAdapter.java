75a0f03c0aa7f76d37d8b3a28d200f5794c97221
VLADIMIR <mgmg2009@yandex.ru>
2018-05-22 14:41:40 +0400

    edit onClickListener in CustomAdapter

--- app/src/main/java/com/example/vladimir/contactreader/CustomAdapter.java
+++ app/src/main/java/com/example/vladimir/contactreader/CustomAdapter.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class CustomAdapter extends CursorRecyclerAdapter<CustomAdapter.ViewHolder>  {
+public class CustomAdapter extends CursorRecyclerAdapter<CustomAdapter.ViewHolder> {
 
     private ItemClickListener itemClickListener;
     private final int KEY = 1;
@@ -32,24 +32,26 @@ public class CustomAdapter extends CursorRecyclerAdapter<CustomAdapter.ViewHolde
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
 
-        View view =  layoutInflater.inflate(R.layout.contacts_list_item, parent, false);
-        return new ViewHolder(view);
+        View view = layoutInflater.inflate(R.layout.contacts_list_item, parent, false);
+
+        final ViewHolder viewHolder = new ViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cursor.moveToPosition(viewHolder.getAdapterPosition());
+                String key = cursor.getString(KEY);
+                itemClickListener.onItemClick(key);
+            }
+        });
+        return viewHolder;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder  {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView namTextView;
 
-        public ViewHolder (View itemView) {
+        public ViewHolder(View itemView) {
             super(itemView);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    cursor.moveToPosition(getAdapterPosition());
-                    String key = cursor.getString(KEY);
-                    itemClickListener.onItemClick(key);
-                }
-            });
             this.namTextView = itemView.findViewById(R.id.item_textView);
         }
     }
